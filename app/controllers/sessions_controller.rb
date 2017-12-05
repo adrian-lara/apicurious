87190eaa01a6cef6a05c245b5c61e61b594350d6
Adrian Lara <adrianblara@gmail.com>
2017-12-05 16:12:25 -0700

    after login, user data is created or found

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,10 @@
 class SessionsController < ApplicationController
 
   def create
-
+    if user = User.return_by_omniauth(request.env["omniauth.auth"])
+      session[:user_id] = user.id
+    end
+    redirect_to user_path
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+
+  def self.return_by_omniauth(auth_data)
+    user = find_or_create_by(uid: auth_data.uid)
+    user.update(user_name: auth_data.info.nickname,
+                token: auth_data.credentials.token
+               )
+    user
+  end
+
 end

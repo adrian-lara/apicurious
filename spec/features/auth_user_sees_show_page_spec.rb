dacb5d07ad4d51315c657e885bff466b1f0015cc
Adrian Lara <adrianblara@gmail.com>
2017-12-05 17:52:01 -0700

    login feature test is more robust

--- spec/features/auth_user_sees_show_page_spec.rb
+++ spec/features/auth_user_sees_show_page_spec.rb
@@ -6,8 +6,12 @@ describe "When a user logs in" do
     click_link "Login"
 
     expect(current_path).to eq(root_path)
+    expect(User.last.uid).to eq("1337")
+    expect(User.last.user_name).to eq("adrian-lara")
+    expect(User.last.token).to eq("1234")
     expect(page).not_to have_link("Login")
-    expect(page).to have_content("Adrian Lara")
     expect(page).to have_content("adrian-lara")
+    
+    expect(page).to have_content("Adrian Lara")
   end
 end

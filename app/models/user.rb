9867ca4320b5d93d28186dbfb185b63035968d78
Adrian Lara <adrianblara@gmail.com>
2017-12-05 23:24:32 -0700

    remove extra white space line in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,7 @@ class User < ApplicationRecord
   def self.return_by_omniauth(auth_data)
     user = find_or_create_by(uid: auth_data.uid)
     user.update(user_name: auth_data.info.nickname,
-                token: auth_data.credentials.token
-               )
+                token: auth_data.credentials.token)
     user
   end
 

bbc362e19d015c882bc71689fce50e65ce422e0c
Adrian Lara <adrianblara@gmail.com>
2017-12-05 23:51:12 -0700

    github_service poro has methods that call to github's public api

--- app/models/github_service.rb
+++ app/models/github_service.rb
@@ -9,4 +9,19 @@ class GithubService
     user_public_data(user_name)["name"]
   end
 
+  def self.image(user_name)
+    user_public_data(user_name)["avatar_url"]
+  end
+
+  def self.bio(user_name)
+    user_public_data(user_name)["bio"]
+  end
+
+  def self.location(user_name)
+    user_public_data(user_name)["location"]
+  end
+
+  def self.blog(user_name)
+    user_public_data(user_name)["blog"]
+  end
 end

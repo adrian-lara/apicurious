e2b9fc286826f7b3ec7e009bd8f8f60d62df02a2
Adrian Lara <adrianblara@gmail.com>
2017-12-05 23:12:26 -0700

    refactor GithubService

--- app/models/github_service.rb
+++ app/models/github_service.rb
@@ -1,9 +1,12 @@
 class GithubService
 
-  def self.full_name(user_name)
+  def self.user_public_data(user_name)
     response = Faraday.get("https://api.github.com/users/#{user_name}")
-    raw_data = JSON.parse(response.body)
-    raw_data["name"]
+    JSON.parse(response.body)
+  end
+
+  def self.full_name(user_name)
+    user_public_data(user_name)["name"]
   end
 
 end

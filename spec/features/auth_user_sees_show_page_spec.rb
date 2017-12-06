0d70d1c4792b02d24bc616d8e299f9f4a76e492d
Adrian Lara <adrianblara@gmail.com>
2017-12-06 15:35:40 -0700

    user log in base spec is rewritten to render that user's show page

--- spec/features/auth_user_sees_show_page_spec.rb
+++ spec/features/auth_user_sees_show_page_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
 
 describe "When a user logs in" do
-  it "they are redirected to their show page that has basic public user information" do
+  it "they are redirected to their own show page and see their basic public information" do
     VCR.use_cassette('base_user_info') do
       visit root_path
       click_link "Login"
 
-      expect(current_path).to eq(root_path)
+      expect(current_path).to eq(user_path(User.last.user_name))
       expect(User.last.uid).to eq("1337")
       expect(User.last.user_name).to eq("adrian-lara")
       expect(User.last.token).to eq("1234")
